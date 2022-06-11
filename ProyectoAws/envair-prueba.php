e362d345a979738b34d846746fc082db8cfe1cf0
SOC2 SV <soc2.sv@intelector.net>
2022-06-11 02:52:57 -0600

    Probando PHPmailer

--- ProyectoAws/envair-prueba.php
+++ ProyectoAws/envair-prueba.php
@@ -1,31 +1,44 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+class sendMailToConfirmSession 
+{
+    public $user="null";
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    function __construct($user) {
+        $user= $user;
+    }
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'jose201400588@gmail.com';                 // SMTP username
-$mail->Password = 'lvzmdypgiuydeisu';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    public function sendMail()
+    {
+        $mail = new PHPMailer;
+        //$mail->SMTPDebug = 3;                               // Enable verbose debug outpu
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'jose201400588@gmail.com';                 // SMTP username
+        $mail->Password = 'lvzmdypgiuydeisu';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('jose201400588@gmail.com', 'Admin');
-$mail->addAddress('jose201400588@gmail.com', 'Admin');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
+        $mail->setFrom('jose201400588@gmail.com', 'Admin');
+        $mail->addAddress('jose201400588@gmail.com');    
 
-$mail->isHTML(true);                                  // Set email format to HTML
+        $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = 'Inicio de sesion';
+        $mail->Body    = "Se ha detectado un inicio de sesion con el usuario $user";
+        $mail->AltBody = '...';
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent';
+        }
+    }
 }
+
+$sendmail = new sendMailToConfirmSession(Juan);
+
+$sendmail.sendMail();
