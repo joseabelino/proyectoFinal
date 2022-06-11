141b5bb6cd2941e2978f4cd2742612c03b262d74
Jose.abelino <201400588@ujmd.edu.sv>
2022-06-11 04:40:50 -0600

    Agregando boton subir

--- forms/index.php
+++ forms/index.php
@@ -54,7 +54,7 @@
           <li><a href="../logout.php" class="nav-link scrollto"><i class="fa fa-mail-reply-all"></i> <span>Salir</span></a></li>
           <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-config"></i> <span>Subir archivo</span></a></li>
           <form action="" method="post" enctype="multipart/form-data">
-          <input type="file" name="archivo" id="archivo" style="width:100%"><br>
+          <input type="file" name="archivo" id="archivo" style="width:100%"><br><br>
           <input type="submit" name="uploadBtn" value="Upload" />
           </form>
         </ul>
