cdd18a2d15e3db96e17d2a44ca4d04153a73aec4
Jose.abelino <201400588@ujmd.edu.sv>
2022-06-11 04:31:36 -0600

    Agregando boton subir

--- forms/index.php
+++ forms/index.php
@@ -53,7 +53,7 @@
           <li><a href="../reset-password.php" class="nav-link scrollto"><i class="fa fa-refresh"></i> <span>Cambiar contraseña</span></a></li>
           <li><a href="../logout.php" class="nav-link scrollto"><i class="fa fa-mail-reply-all"></i> <span>Salir</span></a></li>
           <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Subir archivo</span></a></li>
-          <input type="file" name="archivo" id="archivo" style="width:20%">
+          <input type="file" name="archivo" id="archivo" style="width:100%">
         </ul>
       </nav><!-- .nav-menu -->
     </div>
